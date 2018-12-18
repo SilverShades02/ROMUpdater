0deee0fb67b18b7096e1e69c379b4836bab094f7
Udit Karode <udit.karode@gmail.com>
2018-12-18 11:22:11 +0530

    main.kt: create function for setting up UI
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- app/src/main/java/io/github/uditkarode/updater/Main.kt
+++ app/src/main/java/io/github/uditkarode/updater/Main.kt
@@ -1,17 +1,17 @@
 /*
-Copyright 2018 Udit Karode
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at:
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+   * Copyright 2018 Udit Karode
+   *
+   * Licensed under the Apache License, Version 2.0 (the "License");
+   * you may not use this file except in compliance with the License.
+   * You may obtain a copy of the License at:
+   *
+   * http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
  */
 
 package io.github.uditkarode.updater
@@ -26,22 +26,40 @@ import android.widget.Toast
 import co.revely.gradient.RevelyGradient
 import com.airbnb.lottie.LottieAnimationView
 import com.androidnetworking.AndroidNetworking
+import com.tonyodev.fetch2.*
 import com.topjohnwu.superuser.Shell
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
+
 class Main : AppCompatActivity() {
 
-    private val tvHeader : TextView by lazy { findViewById<TextView>(R.id.header_title) }
-    private val lavNoNotifs : LottieAnimationView by lazy { findViewById<LottieAnimationView>(R.id.nonotifs) }
+    private lateinit var fetch: Fetch
+    private val tvHeader: TextView by lazy { findViewById<TextView>(R.id.header_title) }
+    private val lavNoNotifs: LottieAnimationView by lazy { findViewById<LottieAnimationView>(R.id.nonotifs) }
+
+    private lateinit var downUrl: String // @todo: obtain from json
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupUi()
 
         AndroidNetworking.initialize(applicationContext)
-
         Toast.makeText(this@Main, getProp("ro.build.date.utc"), Toast.LENGTH_LONG).show() //@todo: do something with this
 
+        if(updateAvailable()){
+            fetch = Fetch.getInstance(FetchConfiguration.Builder(this).setDownloadConcurrentLimit(1).build())
+            val request = Request(downUrl, Constants.UPDATE_PACKAGE)
+            request.priority = Priority.HIGH
+            request.networkType = NetworkType.ALL
+        }
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
+    }
+
+    private fun setupUi(){
         RevelyGradient
             .sweep()
             .colors(intArrayOf(Color.parseColor("#FF2525"), Color.parseColor("#6078EA"))) //@todo: find a better gradient ffs
@@ -57,11 +75,12 @@ class Main : AppCompatActivity() {
         }
     }
 
-    override fun attachBaseContext(newBase: Context) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
-    }
-
     private fun getProp(key: String): String {
         return Shell.sh("getprop $key").exec().out[0]
+    }
+
+    private fun updateAvailable() : Boolean {
+        // @todo: check for updates using json file on github (raw)
+        return true
     }
 }
